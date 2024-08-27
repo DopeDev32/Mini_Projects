25b056ff6318173376c599acfea5250cb092f9e4
Debashis Gope <82468701+DopeDev32@users.noreply.github.com>
2024-08-28 01:32:20 +0530

    Update Javascript.js

--- Form Page/Javascript.js
+++ Form Page/Javascript.js
@@ -1,1 +1,12 @@
+document.getElementById('surveyForm').addEventListener('submit', function(e) {
+            e.preventDefault();
+            alert('Thank you for submitting the survey!');
+            this.reset();
+        });
+        const value = document.querySelector("#value");
+        const input = document.querySelector("#pi_input");
+        value.textContent = input.value;
+        input.addEventListener("input", (event) => {
+        value.textContent = event.target.value;
+});
 
